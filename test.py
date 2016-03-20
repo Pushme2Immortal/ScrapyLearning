a9ce650601a62a21a8cabcd6327468d18e80d409
Pushme2Immortal <vcxnew@outlook.com>
2016-03-20 13:34:40 +0800

    BeautifulSoup Sina HomeWork

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
-A = 1
-B = 2
-C = 3
+from bs4 import BeautifulSoup
+
+f = open("out.md", "w")
+
+soup = BeautifulSoup(open("sina2.html"))
+
+tempList = soup.find_all("a")
+
+for element in tempList:
+    if len(element.contents)>0:
+        print >> f,element.contents[0].encode("utf-8")
+    if "href" in element.attrs:
+        print >> f,element["href"].encode("utf-8")
+
+f.close()
